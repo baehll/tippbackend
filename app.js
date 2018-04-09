8b83e24cb1ba760026e7e19714276e9e7eb58b68
baehll <h@a.com>
2018-04-09 14:22:27 +0200

    gods plan

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 let express = require("express");
-let app     = express();
-let fs      = require("fs");
-let path    = require("path");
+let app = express();
+let fs = require("fs");
+let path = require("path");
 let mySqlScript = require(__dirname + "\\db\\mySqlScript");
 
+/*
 app.get("/listUsers", function(req, res){
   fs.readFile(__dirname + "/data/user.json", "utf8", function(err, data){
     console.log(data);
     res.end(data);
   });
 });
+*/
 
 app.get("/matches/:stage/:group", (req, res) => {
-  mySqlScript.parseMatches((err, result) =>{
-    console.log(result);
-    res.end(JSON.stringify(result));
-  }, req.params.stage, req.params.group);
+    mySqlScript.parseMatchesForStageAndGroups((err, result) => {
+        console.log(result);
+        res.end(JSON.stringify(result));
+    }, req.params.stage, req.params.group);
 });
 
-app.get("/players/allPlayers", (req, res) =>{
-  mySqlScript.parsePlayers((err, result) =>{
-    if(err) return console.log(err);
-    res.end(JSON.stringify(result));
-  });
+app.get("/matches/currentMatches", (req, res) => {
+    mySqlScript.parseCurrentMatches((err, result) => {
+        res.end(JSON.stringify(result));
+    });
+});
+
+//get player tips for matches in current stage
+app.get("/players/playerTips/:id", (req, res) => {
+    mySqlScript.parsePlayerTipsForCurrentStage((err, result) => {
+        res.end(JSON.stringify(result));
+    }, req.params.id);
+});
+
+app.post("/players/addPlayer/:name", (req, res) => {
+    mySqlScript.addPlayer((err, result) => {
+        res.end({ "result": "gucci" });
+    }, req.params.name);
+});
+
+app.get("/players/allPlayers", (req, res) => {
+    mySqlScript.parsePlayers((err, result) => {
+        if (err) return console.log(err);
+        res.end(JSON.stringify(result));
+    });
 });
 
 /*
